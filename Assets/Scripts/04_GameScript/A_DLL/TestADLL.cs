65bdd7d7fe63215cb9022b2503e4b03dfacb744e
JiahaoWu <jiahaodev@163.com>
2019-11-27 02:39:05 +0800

    添加程序集定义测试

---
+++ Assets/Scripts/04_GameScript/A_DLL/TestADLL.cs
@@ -0,0 +1,20 @@
+/************************************************************
+    文件: TestADLL.cs
+	作者: JiahaoWu
+    邮箱: jiahaodev@163.com
+    日期: 2019/11/27 2:27:22
+	功能: #TODO#
+*************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestADLL : MonoBehaviour {
+
+    public static void TestADLLFunc() {
+        Debug.Log("TestADLLFunc");
+    }
+
+  
+}
